543be2754cfe935f6438a9e1d9bee57a629db40e
hannahcmtucker <hannahcmtucker@gmail.com>
2017-12-12 18:06:26 +0000

    Add favicon

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.engine(
 );
 
 app.set('port', process.env.PORT || 3000);
-app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.png')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(routes);
 
