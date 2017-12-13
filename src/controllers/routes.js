433da608dca58613ac53558fe74bbfe653e05182
Caroline <carolinelappleby@gmail.com>
2017-12-13 12:27:13 +0000

    wrote getMovies query

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -4,5 +4,11 @@ const express = require('express');
 const router = express.Router();
 const queries = require('./queries');
 
+router.get('/', (req, res, next) => {
+  queries
+    .getMovies()
+    .then(movies =>console.log(movies))
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
--- src/database/dbBuild.sql
+++ src/database/dbBuild.sql
@@ -13,8 +13,8 @@ CREATE TABLE users (
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    year DATE,
-    desc TEXT,
+    year VARCHAR(4) NOT NULL,
+    description TEXT,
     rating FLOAT(2)
 );
 
@@ -23,4 +23,13 @@ CREATE TABLE votes (
     user_id INTEGER REFERENCES users(id)
 );
 
+INSERT INTO users (username, password) VALUES ('Lex', 'horrorshow'), ('Hannah', 'password');
+
+INSERT INTO movies(title, year, description, rating) VALUES
+('I walked with a Zombie', '1943', 'Evocative direction', 9.3),
+('Split', '2017', 'Split serves as a dramatic tour de force for James McAvoy in multiple roles -- and finds writer-director M. Night Shyamalan returning resoundingly to thrilling form.', 7.4),
+('The Conjuring 2', '2016', 'The Conjuring 2 can''t help but lose a bit of its predecessor''s chilly sting through familiarity, but what remains is still a superior ghost story told with spine-tingling skill.', 7.9),
+('The Others', '2001', 'The Others is a spooky thriller that reminds us that a movie doesn''t need expensive special effects to be creepy.', 8.3);
+
+INSERT INTO votes (movie_id, user_id) VALUES (1, 1), (2, 1), (2, 2), (3, 2);
 COMMIT;
