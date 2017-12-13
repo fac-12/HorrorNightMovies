bb4989ee1387bb33e7680d94d574a312d3fbfe9c
hannahcmtucker <hannahcmtucker@gmail.com>
2017-12-13 14:47:43 +0000

    Set up layouts partials

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -4,5 +4,35 @@ const express = require('express');
 const router = express.Router();
 const queries = require('./queries');
 
+const movies = [{
+    id: 1,
+    title: "Titanic",
+    year: 1997,
+    rating: 7.8,
+    count: 1,
+}, {
+    id: 2,
+    title: "Titanic",
+    year: 1997,
+    rating: 7.8,
+    count: 1,
+}]
+
+const singleMovie = {
+    id: 3,
+    title: "Titanic",
+    year: 1997,
+    rating: 7.8,
+    count: 1,
+    description: "Big boat sinks"
+}
+
+router.get('/getMovies', (req, res, next) => {
+    res.render('moviesMain', { movies })
+})
+
+router.get('/getMovieInfo:id', (req, res, next) => {
+    res.render('singleMovie', { singleMovie })
+})
 
 module.exports = router;
