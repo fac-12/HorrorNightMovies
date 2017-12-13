39a3f6a455f276291a05395dc568cda4d1e4a216
Natalie <njseeto@users.noreply.github.com>
2017-12-13 17:23:00 +0000

    addMovie query and route working

--- src/controllers/queries.js
+++ src/controllers/queries.js
@@ -10,9 +10,15 @@ const singleMovieInfo = (id) =>
 db.query(
    `SELECT movies.title, movies.year, movies.description, movies.rating, COUNT(movies.id) FROM movies FULL JOIN votes ON movies.id=votes.movie_id WHERE movies.id=${id} GROUP BY movies.id`);
 
-// const addMovie = ()
+const addMovie = newMovie => {
+  const {title, year, description} = newMovie;
+  return db.query(
+    `INSERT INTO movies(title, year, description) VALUES($1,$2, $3)`,
+    [title, year, description]);
+  };
 
 module.exports = {
   getMovies,
   singleMovieInfo,
+  addMovie,
 }
